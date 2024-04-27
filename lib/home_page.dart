4567fd6a31b957516ee73d6c84d8282ee53b43fd
Vincent Chandra <vincentzhang1277@gmail.com>
2024-04-27 21:33:34 +0700

    membuat appbar mirip cookpad di homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cookpad/widget/textfield_custom.dart';
 import 'package:flutter/material.dart';
 
 class HomePageScreen extends StatefulWidget {
@@ -12,9 +13,36 @@ class _HomePageScreenState extends State<HomePageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        leading: Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Image.asset(
+            'assets/images/cookpad_logo.png',
+            width: 10,
+            height: 10,
+          ),
+        ),
+        title: Container(
+          height: 40,
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 5),
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: IconButton(
+              icon: Icon(Icons.notifications),
+              tooltip: "Notifications",
+              onPressed: () {},
+            ),
+          )
+        ],
       ),
-      body: Text("Home Page LAH"),
     );
   }
 }
